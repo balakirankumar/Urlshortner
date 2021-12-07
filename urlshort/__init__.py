121f40883ee565f50dbdcf7bfd57db7c51c167d4
BalaKiranKumar <53617263+balakirankumar@users.noreply.github.com>
2021-12-07 15:12:59 +0530

    Update __init__.py

--- urlshort/__init__.py
+++ urlshort/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    app=Flask(__name__)
-    app.secret_key='h3hhg23h2g2h222hhghnb'
+    application=Flask(__name__)
+    application.secret_key='h3hhg23h2g2h222hhghnb'
 
 
     from . import urlshort
 
-    app.register_blueprint(urlshort.bp)
+    application.register_blueprint(urlshort.bp)
 
-    return app
+    return application
